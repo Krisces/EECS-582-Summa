c811f71aaff5ddbd5a548aad6fcaa49b1bf4d746
Shravya Matta <mattashravyanaidu@gmail.com>
2025-03-02 15:55:12 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Header from "./_components/Header";
-import Hero from "./_components/Hero";
+import Header from "./_components/Header";  // Importing the Header component from the '_components' folder
+import Hero from "./_components/Hero";  // Importing the Hero component from the '_components' folder
 
-
+// Home component, typically the landing page of the website
 export default function Home() {
   return (
     <div>
-      <title>Welcome to Summa</title>
-      <Header/>
-      <Hero/>
+      <title>Welcome to Summa</title>  {/* Setting the page title for the browser tab */}
+      <Header/>  {/* Rendering the Header component */}
+      <Hero/>  {/* Rendering the Hero component */}
     </div>
   );
 }
