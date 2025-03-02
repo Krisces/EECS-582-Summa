d0bd3c5baac9749220180c4488de09ada1100aaf
realVinayak <realvinayak123@gmail.com>
2025-03-02 12:59:04 -0600

    (docs): Add comments

--- utils/schema.tsx
+++ utils/schema.tsx
@@ -1,3 +1,14 @@
+/**
+Prologue:
+Name of Program: utils/schema.tsx
+Description: Provides schema objects.
+Inputs: None
+Outputs: Exports tables
+Author: Kristin Boeckmann, Zach Alwin, Lisa Phan, Shravya Mehta, Vinayak Jha
+Creation Date: 02/16/2025
+*/
+
+// Import necessary objects
 import { integer, numeric, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
 // Categories Table with Budget Amount
@@ -15,7 +26,7 @@ export const Income = pgTable('income', {
     name: varchar('name').notNull(),
     amount: numeric('amount').notNull().default('0'),
     transactionDate: varchar('transactionDate').notNull(),
-    createdBy: varchar('createdBy').notNull() 
+    createdBy: varchar('createdBy').notNull()
 });
 
 // Expenses Table
@@ -27,5 +38,5 @@ export const Expenses = pgTable('expenses', {
         .references(() => Categories.id)
         .notNull(),  // Reference to the category
     createdAt: varchar('createdAt').notNull(),
-    createdBy: varchar('createdBy').notNull() 
+    createdBy: varchar('createdBy').notNull()
 });
