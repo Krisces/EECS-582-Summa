72264bd2c46eee67642fd66ba852af537487296a
Krisces <kristinboeckmann3@gmail.com>
2025-03-09 15:01:37 -0500

    Update popover.tsx

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -54,7 +54,7 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal> {/* Portal ensures popover renders outside normal DOM hierarchy */}
+  <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
       ref={ref} // Assigns forwarded ref for direct DOM manipulation
       align={align} // Sets the alignment of the popover
@@ -63,11 +63,10 @@ const PopoverContent = React.forwardRef<
         "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
-      {...props} // Passes additional props to Radix Popover Content
+      {...props}  // Passes additional props to Radix Popover Content
     />
   </PopoverPrimitive.Portal>
 ))
-
 // Assign display name for better debugging and dev tools support
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
